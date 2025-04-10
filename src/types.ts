b51f51f1f36fe8670e1a2d98edaa1d1e59d0c1c9
Corina Udrescu <corinutz@gmail.com>
2025-04-10 12:20:29 +0300

    better type for job experience
    using union

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,17 @@
-export type JobExperience = {
+type Job = {
   job_title: string;
   employment_type: string;
   company: string;
-  is_current: boolean;
   start_date: { month: string; year: string };
-  end_date?: { month: string; year: string };
 };
+
+type CurrentJob = Job & {
+  is_current: true;
+};
+
+type PreviousJob = Job & {
+  is_current: false;
+  end_date: { month: string; year: string };
+};
+
+export type JobExperience = CurrentJob | PreviousJob;
